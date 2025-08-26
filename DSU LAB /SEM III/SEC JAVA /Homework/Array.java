f4a5b3d0ad2da0b99fe72776d379313222477792
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:31:36 +0530

    Update Array.java

--- DSU LAB /SEM III/SEC JAVA /Homework/Array.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/Array.java
@@ -1,1 +1,17 @@
+// 20-08-2025
+// Create an array of size 10. Add squares of the numbers to this array. 
 
+class SquaresArray {
+    public static void main(String[] aya) {
+        int[] squares = new int[10];  // array of size 10
+
+        for (int i = 0; i < squares.length; i++) {
+            squares[i] = (i + 1) * (i + 1); 
+        }
+      
+        System.out.println("Squares of numbers :");
+        for (int i = 0; i < squares.length; i++) {
+            System.out.println( i + " = " + squares[i]);
+        }
+    }
+}
