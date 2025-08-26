283fc0d995ae6cad7f2924d46941a963c3461428
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:33:03 +0530

    Update EvenorOdd.java

--- DSU LAB /SEM III/SEC JAVA /Homework/EvenorOdd.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/EvenorOdd.java
@@ -1,1 +1,22 @@
+// 21-08-2025
+// Take one number and print whether it is even or odd.
+import java.util.*;
+class EvenorOdd
+{
+    public static void main(String[] aya) 
+  {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) 
+        {
+            System.out.println(num + " is Even");
+        } 
+        else 
+        {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
