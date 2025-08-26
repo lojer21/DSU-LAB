585b8e89b19815f3031b293e84d1d5f2516f781b
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:35:06 +0530

    Update Greater.java

--- DSU LAB /SEM III/SEC JAVA /Homework/Greater.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/Greater.java
@@ -1,1 +1,24 @@
+// 20-08-2025
+// 1. Take two numbers as input from the user and print the greater one.
+import java.util.*;
+class Greater
+{
+  public static void main(String[] aya)
+  {
+        Scanner lo = new Scanner(System.in);
+        System.out.print("Enter Num1: ");
+        int a = lo.nextInt();
+        System.out.print("Enter Num2: ");
+        int b = lo.nextInt();
+        if (a == b) 
+            System.out.println("Both are equal.");
+        else
+        {
+            if (a > b)
+                System.out.println(a + " is Greater than " + b);
+            else
+                System.out.println(b + " is Greater than " + a);
+        }
+    }
 
+}
