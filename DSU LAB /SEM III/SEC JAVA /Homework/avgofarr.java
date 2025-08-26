9e08faa7e53f1c0aa04c5ddba3ce1c3ed4c22bb8
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:41:57 +0530

    Update avgofarr.java

--- DSU LAB /SEM III/SEC JAVA /Homework/avgofarr.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/avgofarr.java
@@ -1,1 +1,17 @@
-
+// 25/08/2025
+// Find the sum and average of the array elemnts.
+import java.util.*;
+public class arrayinout
+{
+    public static void main(String[] aya)
+    {
+        Scanner lo= new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n =lo.nextInt();
+        int[] arr = new int[n];
+        System.out.printf("Enter %d of the array: ");
+        for (int i = 0; i< n; i++)
+          {
+            arr[i] = input.nextInt();
+    }
+}
