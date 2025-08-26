c7a4c3289277cadb4564c484316c8aad6936505d
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:39:45 +0530

    Update SquareofNum.java

--- DSU LAB /SEM III/SEC JAVA /Homework/SquareofNum.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/SquareofNum.java
@@ -1,1 +1,14 @@
-
+// 21-08-2025
+// 2. Take one number and print its square.
+import java.util.*;
+class SquareofNum 
+{
+    public static void main(String[] aya) 
+  {
+        Scanner lo = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+        int square = num * num;
+        System.out.println("Square of " + num + " is: " + square);
+    }
+}
