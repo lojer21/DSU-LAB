0343ae38f02f9997a6cd00f540b9b87c085d218c
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:37:53 +0530

    Update EvenorOdd.java

--- DSU LAB /SEM III/SEC JAVA /Homework/EvenorOdd.java
+++ DSU LAB /SEM III/SEC JAVA /Homework/EvenorOdd.java
@@ -5,10 +5,10 @@ class EvenorOdd
 {
     public static void main(String[] aya) 
   {
-        Scanner sc = new Scanner(System.in);
+        Scanner lo = new Scanner(System.in);
 
         System.out.print("Enter a number: ");
-        int num = sc.nextInt();
+        int num = lo.nextInt();
 
         if (num % 2 == 0) 
         {
