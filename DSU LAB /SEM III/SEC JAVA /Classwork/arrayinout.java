5951db97085516f1492504134c3eaef667bffd26
LOJER PEDERAR <lojerpederar@gmail.com>
2025-08-26 23:43:46 +0530

    Create arrayinout.java

---
+++ DSU LAB /SEM III/SEC JAVA /Classwork/arrayinout.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class arrayinout
+{
+    public static void main(String[] Args)
+    {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = input.nextInt();
+        int[] arr = new int[n];
+        
+        System.out.printf("Enter %d of the array: ");
+        for (int i = 0; i< n; i++)
+            arr[i] = input.nextInt();
+
+
+    
+    }
+}
