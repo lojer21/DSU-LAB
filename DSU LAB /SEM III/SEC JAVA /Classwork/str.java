b957f8b88a47f230bda91c54c432e5558e870707
LOJER PEDERAR <lojerpederar@gmail.com>
2025-09-01 12:12:46 +0530

    Create str.java

---
+++ DSU LAB /SEM III/SEC JAVA /Classwork/str.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class str
+{
+    public static void main(String[] aya)
+    {
+        Scanner LO= new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String a =LO.next().toUpperCase();
+        System.out.println(a);
+    }
+
+}
