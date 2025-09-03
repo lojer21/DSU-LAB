0a49934005ff7474c8c5c085f3abc6f5d6fb11de
LOJER PEDERAR <lojerpederar@gmail.com>
2025-09-03 12:59:58 +0530

    Update str.java

--- DSU LAB /SEM III/SEC JAVA /Classwork/str.java
+++ DSU LAB /SEM III/SEC JAVA /Classwork/str.java
@@ -1,12 +1,16 @@
+//String
+//01/09/2025
 import java.util.*;
 class str
 {
     public static void main(String[] aya)
     {
         Scanner LO= new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String a =LO.next().toUpperCase();
+        System.out.print("Enter two string: ");
+        String a =LO.next();
         System.out.println(a);
+        String b =LO.next();
+        System.out.println(b);
     }
 
 }
