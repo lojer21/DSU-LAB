7eb328ea270266b4073f33970485e3c89b58d472
LOJER PEDERAR <lojerpederar@gmail.com>
2025-09-15 12:32:48 +0530

    Create RightShift.java

---
+++ DSU LAB /SEM III/SEC JAVA /Classwork/RightShift.java
@@ -0,0 +1,10 @@
+//RightShift
+//---> 08/09/2025 
+public class RightShift
+{
+    public static void main(String[] args){
+        int a = 20;
+        int result=a>>2;
+        System.out.println("a>>2="+result);
+    }
+}
